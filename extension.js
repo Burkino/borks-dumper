00da72a27bb9d4e1fae22d8aaadcffb42b94c231
Burkino <burkino51@gmail.com>
2020-06-12 21:10:02 -0400

    yes

--- extension.js
+++ extension.js
@@ -1,6 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fetch = require('node-fetch');
+
+let dump = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+dump.command = "borks-dumper.dump";
+dump.tooltip = "Dump script";
+dump.text = "$(server-process) Constant dump";
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,16 +19,55 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "borks-dumper" is now active!');
+	dump.show()
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('borks-dumper.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('borks-dumper.dump', function () {
+		const settings = vscode.workspace.getConfiguration('borks-dumper')
+		if (!vscode.window.activeTextEditor || vscode.window.activeTextEditor.document.getText() == "") {
+			vscode.window.showErrorMessage("hey dummy you need to paste an obfuscated script for it to work")
+			return
+		}
+		const fullRange = new vscode.Range(vscode.window.activeTextEditor.document.positionAt(0),vscode.window.activeTextEditor.document.positionAt(vscode.window.activeTextEditor.document.getText().length))
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from borks constant dumper!');
+		fetch('http://borks.club:3000/dumper', {
+			method: "post",
+			headers: {"Content-Type":"application/json"},
+			body: JSON.stringify({
+				code: vscode.window.activeTextEditor.document.getText()
+			  })
+		})
+		.then(res => {
+			if(res.status === 404) {
+				throw new Error()
+			}
+			return res.text()
+		})
+		.then(text => {
+			if (text.includes("No Obfuscator Detected")) {
+				vscode.window.showErrorMessage('Either something wrong with regex-ing or the obfuscator is unsupported (yet)')
+				return
+			}
+
+			if (settings['OutputType'] == 'Create new file') {
+				vscode.workspace.openTextDocument({"content":`${text}`,"language":"lua"})
+				vscode.window.showInformationMessage("Dumped, new tab opened")
+
+			} else if (settings['OutputType'] == 'Replace current file') {
+				vscode.window.activeTextEditor.edit(editBuilder => {editBuilder.replace(fullRange, text)})
+				vscode.window.showInformationMessage("Dumped")
+
+			} else if (settings['OutputType'] == 'Copy to clipboard') {
+				vscode.env.clipboard.writeText(text)
+				vscode.window.showInformationMessage("Dumped, copied to clipboard")
+			}
+		})
+		.catch(function() {
+			vscode.window.showErrorMessage('Something went wrong, the vps probably went dead')
+		})
+
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,7 +75,9 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	dump.dispose()
+}
 
 module.exports = {
 	activate,
